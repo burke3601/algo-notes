dec670e183497396b5e68813cca72682caae467c
Will Burke <billyburke1012@gmail.com>
2021-04-20 10:48:27 -0400

    dll pop

---
+++ dataStructures/doublyLinkedLists/dll-push.js
@@ -0,0 +1,37 @@
+//Push
+
+class Node{
+    constructor(val){
+        this.val = val;
+        this.next = null;
+        this.prev = null;
+    }
+}
+
+class DoublyLinkedList {
+    constructor(){
+        this.head =  null;
+        this.tail = null;
+        this.length = 0;
+    }
+    push(val){
+        var newNode = new Node(val);
+        if(this.length === 0){
+            this.head = newNode;
+            this.tail = newNode;
+        } else {
+            this.tail.next = newNode;
+            newNode.prev = this.tail;
+            this.tail = newNode;
+        }
+        this.length++;
+        return this;
+    }
+}
+
+list = new DoublyLinkedList
+list.push(34);
+list.push(44);
+list.push("Last item!!!!!");
+
+console.log(list)
---
+++ dataStructures/doublyLinkedLists/dll.md
@@ -0,0 +1,8 @@
+# Doubly Linked Lists
+
+- almost identical to SLL. except every node has another pointer, to the previous node.
+- more flexibility than SLL, but it uses more memory
+
+
+
+
